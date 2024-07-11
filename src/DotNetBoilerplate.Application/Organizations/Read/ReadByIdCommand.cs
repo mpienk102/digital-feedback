82fd63eee77c08cbe4e9d0cdd4b7decc736e050f
Paweł Najdecki <rystor69@gmail.com>
2024-07-11 10:29:49 +0200

    Added getting organization by id

---
+++ src/DotNetBoilerplate.Application/Organizations/Read/ReadByIdCommand.cs
@@ -0,0 +1,7 @@
+﻿using DotNetBoilerplate.Shared.Abstractions.Commands;
+using System.Windows.Input;
+
+namespace DotNetBoilerplate.Application.Organizations.Read
+{
+    public record ReadByIdCommand(Guid Id) : ICommand<OrganizationDto>;
+}
---
+++ src/DotNetBoilerplate.Core/Organizations/Exceptions/OrganizationIsNullException.cs
@@ -0,0 +1,9 @@
+﻿using DotNetBoilerplate.Shared.Abstractions.Exceptions;
+
+namespace DotNetBoilerplate.Core.Organizations.Exceptions
+{
+    public sealed class OrganizationIsNullException(Guid Id) : CustomException($"Organization {Id} does not exist")
+    {
+
+    }
+}
