5bf66a20917f566c2e249ec7748edcd8aae60866
Kuba Brzuchacz <47571649+JcobixPL@users.noreply.github.com>
2024-07-15 14:46:46 +0200

    Added Employee class

---
+++ src/DotNetBoilerplate.Core/Employees/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DotNetBoilerplate.Core.Employees
+{
+    public class Employee
+    {
+        private Employee(Guid id, Guid organizationId)
+        {
+            Id = id;
+            UserId = id;
+            OrganizationId = organizationId;
+            Role = RoleInOrganization.None;
+        }
+
+        public Employee()
+        {
+            Id = Guid.NewGuid();
+            UserId = Guid.Empty; 
+            OrganizationId = Guid.Empty;
+            Role = RoleInOrganization.None;
+        }
+
+        public Guid Id { get; private set; }
+        public Guid UserId { get; private set; }
+        public Guid OrganizationId { get; private set; }
+        public RoleInOrganization Role { get; private set; }
+
+        public enum RoleInOrganization
+        {
+            None, // Defaultowy użytkownik bez uprawnień
+            Moderator, // Uprawnienia do może dodawawania pracowników do projektu, może dodawać i edytować projekty
+            Admin // Zarządza organizacją, projektami i ich członkami
+        }
+
+        public void UpdateRole(RoleInOrganization roleInOrganization)
+        {
+            if (roleInOrganization == RoleInOrganization.None) { Role = RoleInOrganization.None; }
+            else if (roleInOrganization == RoleInOrganization.Moderator) { Role = RoleInOrganization.Moderator; }
+            else if (roleInOrganization == RoleInOrganization.Admin) { Role = RoleInOrganization.Admin; }
+        }
+
+        public static Employee Create(Guid userId, Guid organizationId, RoleInOrganization role)
+        {
+            if (organizationId != Guid.Empty)
+                throw new Exception("User already belongs to organization");
+
+            return new Employee(userId, organizationId)
+            {
+                Role = role
+            };
+        }
+
+        public static Employee New(Guid id, Guid organizationId)
+        {
+            return new Employee(id, organizationId);
+        }
+    }
+}
