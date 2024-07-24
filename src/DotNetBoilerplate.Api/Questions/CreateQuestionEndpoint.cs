24307b221b5551fd792e57101d8005e6bc1e9f01
Paweł Najdecki <rystor69@gmail.com>
2024-07-24 17:20:02 +0200

    Added question type in request

--- src/DotNetBoilerplate.Api/Questions/CreateQuestionEndpoint.cs
+++ src/DotNetBoilerplate.Api/Questions/CreateQuestionEndpoint.cs
@@ -1,5 +1,8 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using DotNetBoilerplate.Application.Questions.Create;
+using DotNetBoilerplate.Core.Employees;
+using DotNetBoilerplate.Core.Questions;
 using DotNetBoilerplate.Shared.Abstractions.Commands;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
@@ -21,7 +24,7 @@ namespace DotNetBoilerplate.Api.Questions
             CancellationToken ct
         )
         {
-            var command = new CreateQuestionCommand(request.formId, request.questionText);
+            var command = new CreateQuestionCommand(request.formId, request.questionText, request.questionType);
 
             var result = await commandDispatcher
                 .DispatchAsync<CreateQuestionCommand, Guid>(command, ct);
@@ -37,6 +40,7 @@ namespace DotNetBoilerplate.Api.Questions
         {
             [Required] public Guid formId { get; init; }
             [Required] public string questionText { get; init; }
+            [JsonConverter(typeof(JsonStringEnumConverter))] public QuestionTypeInForm.Type questionType { get; init; }
         }
     }
 }
